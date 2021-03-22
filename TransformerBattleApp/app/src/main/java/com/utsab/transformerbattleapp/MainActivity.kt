3130da37b74a2a535c28610d4739aab778d44458
Utsab <utsab@Utsabs-Mac-mini.local>
2021-03-22 19:17:24 -0400

    -Project Initialization

---
+++ TransformerBattleApp/app/src/main/java/com/utsab/transformerbattleapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.utsab.transformerbattleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
